e6eae84c1431e51a1cf50e28b75bc085ed56b8fb
Shahjada <shahjada.talukdar@gmail.com>
2016-08-07 13:54:58 +0600

    contact-controller.js

---
+++ app/js/contact-controller.js
@@ -0,0 +1,16 @@
+﻿'use strict';
+
+require([
+	'app/js/app',
+	'app/js/person-service'
+], function (app) {
+
+	var contactCtrl = function ($scope , $state, $stateParams) {
+
+    console.log('contactCtrl is running...');
+
+};
+
+  app.register.controller('contactCtrl', ['$scope','$state','$stateParams', contactCtrl]);    
+
+});
