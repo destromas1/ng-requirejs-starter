115326c8c831013aca34da900b3435f080fd5a4c
Shahjada <shahjada.talukdar@gmail.com>
2016-08-07 14:17:14 +0600

    dashboard-controller.js

---
+++ app/js/dashboard-controller.js
@@ -0,0 +1,24 @@
+﻿'use strict';
+
+
+require([
+    'angular',
+    'app/js/app',
+    'app/js/currency-directive',
+    'app/js/phone-directive'
+], function (angular , app) {
+
+    var dashboardCtrl = function ($scope , $state, $stateParams) {
+        console.log('dashboardCtrl is running...');
+
+        $scope.passNameToProfile = function () {
+            $state.go('app.url', { url: 'profile', data: { dashboardName: $scope.dashboardName } });
+        };
+        
+    };
+
+    app.register.controller('dashboardCtrl', ['$scope','$state','$stateParams', dashboardCtrl]);    
+
+    });
+
+
